0db8da7a47e6320fd6664a53a502df53fcc21783
bufdev <4228796+bufdev@users.noreply.github.com>
2022-09-12 11:46:36 -0400

    Add fathom (#1)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,15 @@ const config = {
   organizationName: "bufbuild", // Usually your GitHub org/user name.
   projectName: "protobuf.com", // Usually your repo name.
 
+  scripts: [
+    {
+      src: "https://sparkling-delightful.protobuf.com/script.js",
+      "data-site": "NXKPYXNZ",
+      "data-spa": "auto",
+      defer: true
+    }
+  ],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
